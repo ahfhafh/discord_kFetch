c9b02cd7b6be148d30976329f6e0c62163bb1d3b
Jack <ahfhafh@gmail.com>
2022-10-05 13:28:51 -0600

    add song to playlist on quiz answer

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,30 +1,27 @@
+const PlaylistManager = require('../spotify/playlistmgr');
+
+const addSong = async function addSongToPlaylist(name) {
+	const playlists = new PlaylistManager();
+	const url = await playlists.getSongFromName(name);
+	return await playlists.addToCurrent(url);
+};
+
 module.exports = {
 	name: 'messageUpdate',
-	execute(oldmsg, message) {
-		console.log('fe');
-		if (message.author.id === '508759831755096074') {
-			if (message.embeds) {
-				const embed = message.embeds[0];
-				console.log(embed);
-				if (embed.description) {
-					console.log(embed);
-					return message.channel.send(embed.title);
+	async execute(_, message) {
+		// message from kpmq bot and is embed
+		if (message.author.id === '508759831755096074' && message.embeds) {
+			const embed = message.embeds[0];
+			// message is song result
+			if (embed.description && embed.title.indexOf('wins!') === -1) {
+				const song_name = embed.title.replace(/["]+/g, '').slice(0, -7);
+				const songAdded = await addSong(song_name);
+				if (songAdded.error) {
+					const msg = songAdded.error.message || songAdded.error;
+
+					return message.channel.send('Error (' + song_name + '): ' + msg);
 				}
 			}
 		}
 	},
 };
-
-// client.on('messageUpdate', (_, message) => {
-// 	console.log('fe');
-// 	if (message.author.id === '508759831755096074') {
-// 		if (message.embeds) {
-// 			const embed = message.embeds[0];
-// 			console.log(embed);
-// 			if (embed.description) {
-// 				console.log(embed);
-// 				return message.channel.send(embed.title);
-// 			}
-// 		}
-// 	}
-// });
